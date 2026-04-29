7e6d935acc6e7a0afc22978ebab1c5444e206dc6
Guilherme F Silveira <gfsilveira@gmail.com>
2026-04-29 08:29:31 -0300

    Editando o acesso aos notebooks

--- dev/script/alunos/pymail.py
+++ dev/script/alunos/pymail.py
@@ -1,19 +1,33 @@
-import ezgmail
+import ezgmail as eg
+from time import sleep
 
-
-def send_email():
-    email = 'guilherme.silveira@fiocruz.br'
-    subject = 'EZGmail Test'
-    mensagem = 'Olá, este é um teste de envio de email usando a biblioteca EZGmail.'
+def send_email_base(
+        conta: int,
+        email: str = 'guilherme.silveira@fiocruz.br',
+        subject: str = 'EZGmail Test',
+        mensagem: str = 'Olá, email teste enviado usando a biblioteca EZGmail.'
+    ) -> None:
+    '''
+        Envia um email usando a biblioteca EZGmail.
+            Args:
+                email (str): O endereço de email do destinatário.
+                subject (str): O assunto do email.
+                mensagem (str): O corpo do email.
+            Returns:
+                None: Esta função não retorna nada.
+    '''
     text = f'''
-    Cabeça 2
-
+    Cabeça
+    {conta}
     {mensagem}
 
     Rabo
     '''
-
-    ezgmail.send(email, subject, text)
+    eg.send(email, subject, text)
+    return None
 
 if __name__ == '__main__':
-    send_email()
+    for i in range(1, 4):
+        print(f'Enviando email {i}...')
+        send_email_base(conta=i)
+        sleep(20)
